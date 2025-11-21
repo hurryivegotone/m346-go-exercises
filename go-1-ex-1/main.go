6455aed3f0592c7be8250a6b9d4b9ed2a2741726
Unknown <unknown@spacegroove.com>
2025-11-21 15:20:14 +0100

    feature/rune task solved

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+var firstName, lastName string = "Gregor", "Samsa"
+var dayOfBirth, monthOfBirth, yearOfBirth int16 = 7, 3, 1884
+var numberOfSiblings int8 = 1
+var heightInMeters float32 = 1.76
+var zodiacSign rune = '\u2653'
+
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
