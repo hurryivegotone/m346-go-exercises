9afeb846ac3bf6608ae0274a0c3303a9bacce0a8
Unknown <unknown@spacegroove.com>
2025-11-28 11:16:37 +0100

    feature/otherzodiacfunctions

--- go-3-ex-1/main.go
+++ go-3-ex-1/main.go
@@ -10,9 +10,9 @@ const (
 	Leo         = '\u264c' // Löwe
 	Virgo       = '\u264d' // Jungfrau
 	Libra       = '\u264e' // Waage
-	Scorpius    = '\u264f' // Skorpion
+	Scorpio     = '\u264f' // Skorpion
 	Sagittarius = '\u2650' // Schütze
-	Capricornus = '\u2651' // Steinbock
+	Capricon    = '\u2651' // Steinbock
 	Aquarius    = '\u2652' // Wassermann
 	Pisces      = '\u2653' // Fische
 )
@@ -20,8 +20,33 @@ const (
 func outputWithZodiacSign(p Person) {
 	var zodiacSign rune = '?'
 
-	if (p.Month == 3 && p.Day >= 21) || (p.Month == 4 && p.Day <= 19) {
+	// https://www.yourzodiacsign.com/calendar/2025/
+	if p.Month == 1 && p.Day < 19 {
+		zodiacSign = Capricon
+	} else if p.Month == 2 && p.Day < 18 {
+		zodiacSign = Aquarius
+	} else if p.Month == 3 && p.Day < 20 {
+		zodiacSign = Pisces
+	} else if p.Month == 4 && p.Day < 19 {
 		zodiacSign = Aries
+	} else if p.Month == 5 && p.Day < 20 {
+		zodiacSign = Taurus
+	} else if p.Month == 6 && p.Day < 21 {
+		zodiacSign = Gemini
+	} else if p.Month == 7 && p.Day < 22 {
+		zodiacSign = Cancer
+	} else if p.Month == 8 && p.Day < 22 {
+		zodiacSign = Leo
+	} else if p.Month == 9 && p.Day < 22 {
+		zodiacSign = Virgo
+	} else if p.Month == 10 && p.Day < 23 {
+		zodiacSign = Libra
+	} else if p.Month == 11 && p.Day < 22 {
+		zodiacSign = Scorpio
+	} else if p.Month == 12 && p.Day < 21 {
+		zodiacSign = Sagittarius
+	} else {
+		zodiacSign = Capricon
 	}
 
 	fmt.Printf("%s %s, born on %02d.%02d.%04d, has the zodiac sign %c.\n",
